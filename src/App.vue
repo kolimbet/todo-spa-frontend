4ee972975f8cf959c0e15068e049fa6295939358
ДК <kolimbet-dv@yandex.ru>
2023-05-26 14:15:26 +0300

    init base VueJS SPA project config

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>It`s work!</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
+    "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
